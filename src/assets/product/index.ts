66e651a9e1043602a48e8b65cbbb011d9015189b
oleksii vovnenko <kespa71for@gmail.com>
2022-12-21 00:03:06 +0300

    fake data

---
+++ src/assets/product/index.ts
@@ -0,0 +1,12 @@
+import product1 from './p1product.jpg';
+import product2 from './p2product.jpg';
+import product3 from './p3product.jpg';
+import product4 from './p4product.jpg';
+import product5 from './p5product.jpg';
+import product6 from './p6product.jpg';
+import product7 from './p7product.jpg';
+import product8 from './p8product.jpg';
+
+export {
+    product1, product3, product4, product5, product6, product8, product7, product2
+}
---
+++ src/data/data.ts
@@ -0,0 +1,69 @@
+interface data {
+    id: number,
+    name: string,
+    img?: any
+    price?: number,
+    action?: number,
+    type: string
+}
+
+export const data: data[] = [
+    {
+        id: 1,
+        img: null,
+        name: 'Adicolor Classics Joggers',
+        type: 'Dress',
+        price: 63.85
+    },
+    {
+        id: 2,
+        img: null,
+        name: 'Nike Sportswear Futura Luxe',
+        type: 'Bag',
+        price: 130.00
+    },
+    {
+        id: 3,
+        img: null,
+        name: 'Yellow Reserved Hoodie',
+        type: 'Dress',
+        price: 364.00,
+        action: 155.00
+    },
+    {
+        id: 4,
+        img: null,
+        name: 'Basic Dress Green',
+        type: 'Dress',
+        price: 236.00
+    },
+    {
+        id: 5,
+        img: null,
+        name: 'Nike Air Zoom Pegasus',
+        type: 'Shoe',
+        price: 220.00,
+        action: 198.00
+    },
+    {
+        id: 6,
+        img: null,
+        name: 'Nike Repel Miler',
+        type: 'Dress',
+        price: 120.50
+    },
+    {
+        id: 7,
+        img: null,
+        name: 'Polarised Sunglasses',
+        type: 'Glasses',
+        price: 160.00
+    },
+    {
+        id: 8,
+        img: null,
+        name: 'Geometric print Scarf',
+        type: 'Scarf',
+        price: 53.00
+    },
+]
