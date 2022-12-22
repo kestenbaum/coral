72d82dfd9134cd2ff41fd4cfb47c47b6fcbdb9ed
oleksii vovnenko <kespa71for@gmail.com>
2022-12-23 00:07:32 +0300

    fix MainBtn

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -7,7 +7,7 @@ interface MainButton extends React.DetailedHTMLProps<React.BaseHTMLAttributes<HT
 
 export const MainButton: FC<MainButton> = ({children}) => {
     return (
-        <button className='flex gap-2 p-4 bg-[#1E2832] px-4 items-center max-w-[250px]'>
+        <button className='flex gap-2 p-4 bg-[#1E2832] px-4 items-center w-full sm:max-w-[250px]'>
             <img src={icon} alt="icon-shop"/>
             <span className='text-white font-sans'>{children}</span>
         </button>
