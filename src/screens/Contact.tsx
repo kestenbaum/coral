2cffcbc534ef8dc244423941af9d7d5c494b3012
oleksii vovnenko <kespa71for@gmail.com>
2022-12-23 00:22:45 +0300

    fix conyact

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -1,13 +1,13 @@
 import React, {FC} from 'react';
 import {Logo} from "../components/Logo";
-import {facebook, dribble, twitter, linkedin} from '../assets/icons/index'
+import {dribble, facebook, linkedin, twitter} from '../assets/icons/index'
 
 
 export const Contact: FC = () => {
     const catalog: string[] = ['Necklaces', 'Hoodies', 'Jewelry Box', 'T-Shirt', 'Jacket']
     const about: string[] = ['Our Producers', 'Sitemap', 'FAQ', 'About Us', 'Terms & Conditions']
     const services: string[] = ['Contact Us', 'Track Your Order', 'Product Care & Repair', 'Book an Appointment', 'Shipping & Returns']
-    const social:any[] = [facebook, dribble, twitter, linkedin]
+    const social: any[] = [facebook, dribble, twitter, linkedin]
 
 
     return (
@@ -20,25 +20,25 @@ export const Contact: FC = () => {
                         </div>
                         <span>Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua</span>
                         <div className='grid grid-cols-4'>
-                            {social.map(social => <img src={social} alt="social-icon"/>)}
+                            {social.map(social => <img key={social} src={social} alt="social-icon"/>)}
                         </div>
                     </div>
                     <div>
                         <h2 className='mb-[30px] font-sans font-bold'>CATALOG</h2>
                         <ul className='flex flex-col gap-[12px]'>
-                            {catalog.map(string => <li>{string}</li>)}
+                            {catalog.map(string => <li key={string}>{string}</li>)}
                         </ul>
                     </div>
                     <div>
                         <h2 className='mb-[30px] font-sans font-bold'>ABOUT US</h2>
                         <ul className='flex flex-col gap-[12px]'>
-                            {about.map(string => <li>{string}</li>)}
+                            {about.map(string => <li key={string}>{string}</li>)}
                         </ul>
                     </div>
                     <div>
                         <h2 className='mb-[30px] font-sans font-bold'>CUSTOMER SERVICES</h2>
                         <ul className='flex flex-col gap-[12px]'>
-                            {services.map(string => <li>{string}</li>)}
+                            {services.map(string => <li key={string}>{string}</li>)}
                         </ul>
                     </div>
                 </div>
