33cf1602a3ad03d9b0b702edba5a2d86d9649696
oleksii vovnenko <kespa71for@gmail.com>
2022-12-23 00:40:56 +0300

    fix products and seller

--- src/screens/Products.tsx
+++ src/screens/Products.tsx
@@ -8,19 +8,17 @@ export const Products: FC = () => {
     return (
         <section className='max-w-[1280px] mx-auto px-4 py-[100px]'>
             <h2 className='w-full text-center text-black text-3xl font-bold font-sans'>New Products</h2>
-            <div className='flex justify-between'>
+            <div className='flex justify-between items-center'>
                 <div className='flex gap-2'>
                     <span className='text-[#777777] text-base cursor-pointer'>All Products</span>
                     <span className='text-[#777777] text-base cursor-pointer'>T-Shirt</span>
                     <span className='text-[#777777] text-base cursor-pointer'>Hoodies</span>
                     <span className='text-[#777777] text-base cursor-pointer'>Jacket</span>
                 </div>
-                <div>
-                    <span className='flex items-center gap-4 px-[15px] py-[5px] mb-[35px] bg-[#1E2832] cursor-pointer'>
+                    <span className='flex items-center gap-4 px-[15px] py-[5px] mb-0 sm:mb-[35px] bg-[#1E2832] cursor-pointer'>
                         <img src={filterIcon} alt="filter" className='max-w-[12px] max-h-[12px]'/>
                         <span className='text-white'>Filter</span>
                     </span>
-                </div>
             </div>
             <div className='flex flex-wrap gap-2 justify-center sm:justify-between'>
                 {data.map(card =>
